45c94b3c96d9f5b05108dccb833a9bee277c847f
otash8110 <otash_baxadirov@mail.ru>
2022-12-23 13:07:45 +0500

    Implement weather fetching

---
+++ Task2/WeatherReponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Task2
+{
+    public class WeatherResponse
+    {
+        public WeatherMainElements Main { get; set; }
+    }
+
+    public class WeatherMainElements
+    {
+        public float Temp { get; set; }
+    }
+}
