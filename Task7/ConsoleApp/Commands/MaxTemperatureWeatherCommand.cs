3542f3afe544d9c6b8e8e05cae84c76519d01ba8
otash8110 <otash_baxadirov@mail.ru>
2023-01-13 16:04:59 +0500

    feat: add command for max temperature

---
+++ Task7/ConsoleApp/Commands/MaxTemperatureWeatherCommand.cs
@@ -0,0 +1,28 @@
+﻿using BL;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp.Commands
+{
+    public class MaxTemperatureWeatherCommand : ICommand
+    {
+        private readonly IConfiguration configuration;
+        private readonly IWeatherService weatherService;
+        public MaxTemperatureWeatherCommand(IWeatherService weatherService,
+            IConfiguration configuration)
+        {
+            this.weatherService = weatherService;
+            this.configuration = configuration;
+        }
+
+        public async Task Execute()
+        {
+            Console.WriteLine("Enter city names separated with comma to fetch a weather info:");
+            var cityNames = Console.ReadLine();
+            var splitedCityNames = cityNames.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            //var weatherResult = await 
+
+            //Console.WriteLine(weatherResult);
+            Console.WriteLine();
+        }
+    }
+}
