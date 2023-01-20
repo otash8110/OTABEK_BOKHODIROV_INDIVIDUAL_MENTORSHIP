51b618a4a5ef93eb36ce376bb259684d1df2a538
otash8110 <otash_baxadirov@mail.ru>
2023-01-20 16:35:10 +0500

    init Task 8

---
+++ Task8/BL.Tests/InputStringValidationTests.cs
@@ -0,0 +1,37 @@
+﻿using BL.Validation;
+
+namespace BL.Tests
+{
+    public class InputStringValidationTests
+    {
+        [Fact]
+        public void ValidateCityName_NotEmptyStringPassed_ReturnsFalse()
+        {
+            var validationService = new ValidationService();
+
+            var result = validationService.ValidateCityName("Tashkent");
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ValidateCityName_EmptyStringPassed_ReturnsTrue()
+        {
+            var validationService = new ValidationService();
+
+            var result = validationService.ValidateCityName("");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ValidateCityName_NullPassed_ReturnsTrue()
+        {
+            var validationService = new ValidationService();
+
+            var result = validationService.ValidateCityName(null);
+
+            Assert.True(result);
+        }
+    }
+}
