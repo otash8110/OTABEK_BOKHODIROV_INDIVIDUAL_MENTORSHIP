3e4430e56f97e1c3f662e18459af0417522968b3
otash8110 <otash_baxadirov@mail.ru>
2023-02-06 14:32:01 +0500

    add EF packagase, configure DbContext and DI services

---
+++ Task10/DAL/ConfigureServices.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Persistent;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddDALServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("Default"),
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+            });
+
+            return services;
+        }
+    }
+}
