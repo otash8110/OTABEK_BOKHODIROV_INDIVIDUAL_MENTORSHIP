a4fc1fffed725d2160704fa5167ec1ae4b755183
otash8110 <otash_baxadirov@mail.ru>
2023-02-20 14:57:39 +0500

    Init and implement Task 13

---
+++ Task13/API/Services/TokenService.cs
@@ -0,0 +1,33 @@
+﻿using IdentityModel.Client;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+
+        public TokenService()
+        {
+
+        }
+
+        public async Task<TokenResponse> GetToken(string scope, string email, string password)
+        {
+            using var client = new HttpClient();
+            var discoveryDocument = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                UserName = email,
+                Password = password,
+                Scope = scope,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Address = discoveryDocument.TokenEndpoint
+            });
+            if (tokenResponse.IsError)
+            {
+                throw new Exception("Token Error");
+            }
+            return tokenResponse;
+        }
+    }
+}
---
+++ Task13/ConsoleApp/Commands/FutureWeatherCommand.cs
@@ -0,0 +1,27 @@
+﻿using BL;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp.Commands
+{
+    internal class FutureWeatherCommand : ICommand
+    {
+        private readonly IWeatherService weatherService;
+        public FutureWeatherCommand(IWeatherService weatherService)
+        {
+            this.weatherService = weatherService;
+        }
+        public async Task Execute()
+        {
+            Console.WriteLine("Enter city name to fetch a weather info:");
+            var cityName = Console.ReadLine();
+            Console.WriteLine("Enter number of days");
+            var days = Convert.ToInt32(Console.ReadLine());
+
+            var weatherResult = await weatherService.GetFutureWeatherByCityNameAsync(cityName,
+                days);
+
+            Console.WriteLine(weatherResult);
+            Console.WriteLine();
+        }
+    }
+}
